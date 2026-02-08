7c7a5d5eae47bb444470a4954460462e47b13b53
Mgalvaz <mgalvaz01@gmail.com>
2026-02-08 22:22:52 +0100

    Crear variante rápida shamir

--- Shamir.py
+++ Shamir.py
@@ -1,5 +1,5 @@
 from typing import Sequence
-from warnings import deprecated
+import time
 
 import galois
 import numpy as np
@@ -22,7 +22,7 @@ class Shamir:
             raise ValueError(f'El numero de participantes ({n}) debe ser menor que el orden del cuerpo de trabajo ({self._cuerpo.order}).')
         if  not  0 <= anticipadas < self._reconstruccion:
             raise ValueError(f'El numero de participaciones anticipadas ({anticipadas}) debe estar entre 0 y el parámetro de privacidad ({self._reconstruccion - 1}).')
-        idx = np.random.default_rng().choice(range(1, self._cuerpo.order), size=n, replace=False)
+        idx = np.arange(1, n+1) # idx = np.random.default_rng().choice(range(1, self._cuerpo.order), size=n, replace=False)
         self.__participantes = self._cuerpo(idx)
         if anticipadas:
             self.__participaciones_anticipadas = zip(self.__participantes[0:anticipadas], self._cuerpo.Random(anticipadas))
@@ -51,39 +51,40 @@ class Shamir:
             polinomio = Poly([*self._cuerpo.Random(self._reconstruccion-1), secreto], field=self._cuerpo)
         return zip(self.__participantes, polinomio(self.__participantes))
 
-    @deprecated('Usar recuperar_cecreto_v3 en su lugar')
     def recuperar_secreto_v1(self, participaciones: Sequence[tuple[ArrayLike, ArrayLike]]) -> ArrayLike:
         if len(participaciones) < self._reconstruccion:
             raise ValueError('No se han proporcionado suficientes participaciones para recuperar el secreto')
-        puntos, valores = zip(*participaciones)
+        puntos, valores = zip(*participaciones[:self._reconstruccion])
         puntos = self._cuerpo(puntos)
         valores = self._cuerpo(valores)
         polinomio = galois.lagrange_poly(puntos, valores)
         return polinomio(0)
 
-    @deprecated('Usar recuperar_cecreto_v3 en su lugar')
     def recuperar_secreto_v2(self, participaciones: Sequence[tuple[ArrayLike, ArrayLike]]) -> ArrayLike:
         if len(participaciones) < self._reconstruccion:
             raise ValueError('No se han proporcionado suficientes participaciones para recuperar el secreto')
-        puntos, valores = zip(*participaciones)
+        puntos, valores = zip(*participaciones[:self._reconstruccion])
         puntos = self._cuerpo(puntos)
         valores = self._cuerpo(valores)
-        polinomio = galois.lagrange_poly(puntos, valores)
-        return polinomio(0)
-
-
-
-
-
-
-
+        mascara = ~np.eye(self._reconstruccion, dtype=bool)
+        coef = np.empty(self._reconstruccion, dtype=int)
+        for j in range(self._reconstruccion):
+            denominador = np.prod(puntos[mascara[j]] - puntos[j])
+            numerador = np.prod(puntos[mascara[j]])
+            coef[j] = numerador / denominador
+        return np.sum(valores * self._cuerpo(coef))
 
 if __name__ == '__main__':
-    gf = GF(2**10)
-    print(gf(3))
+    secreto = bytes(input('Escriba el secreto: '), 'utf-8')
+    if len(secreto) > 8:
+        print(f'Warning: se han mandado más de 8 bytes, se procede a truncar')
+        secreto = secreto[:8]
+
+    gf = GF(2**64)
     sh = Shamir(gf ,3)
-    sh.crear_participantes(15, anticipadas=2)
-    participantes = list(sh.crear_participaciones(gf(950)))
-    print(sh.recuperar_secreto_v1(participantes))
+    sh.crear_participantes(15)
+    participantes = list(sh.crear_participaciones(gf(4782391)))
+    sec2 = sh.recuperar_secreto_v2(participantes)
+    print('secreto', sec2)
 
 
