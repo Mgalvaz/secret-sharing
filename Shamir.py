b9abdb4d527ea8a0f51a1599e6164f31f295dc5f
Mgalvaz <mgalvaz01@gmail.com>
2026-02-18 20:45:20 +0100

    Crear funciones utilidad y cabiar participacoines a base64

--- Shamir.py
+++ Shamir.py
@@ -1,73 +1,78 @@
-from dataclasses import field
 from typing import Sequence
-import time
 import secrets
+from base64 import b64encode, b64decode
 
 import galois
 import numpy as np
 from galois import GF, Poly
 from galois.typing import ArrayLike
 
+from utils import int_a_b64str, b64str_a_int, gf_a_b64str,int_a_str
+
 
 class Shamir:
-    def __init__(self, cuerpo: GF, r: int, n: int) -> None:
+    def __init__(self, cuerpo: GF, r: int, participantes: ArrayLike):
         if n >= cuerpo.order:
-            raise ValueError(f'El numero de participantes ({n}) debe ser menor que el orden del cuerpo de trabajo ({cuerpo.order}).')
+            raise ValueError(f'El numero de participantes ({len(participantes)}) debe ser menor que el orden del cuerpo de trabajo ({cuerpo.order}).')
         if r > n:
-            raise ValueError(f'El parámetro de reconstrucción ({r}) debe ser menor o igual que el número de participantes ({n}).')
-        self._num_participantes = n
+            raise ValueError(f'El parámetro de reconstrucción ({r}) debe ser menor o igual que el número de participantes ({len(participantes)}).')
+        self._participantes = participantes
         self._cuerpo = cuerpo
         self._reconstruccion = r
         self.__participaciones_anticipadas = None
 
-        # Elegir elementos aleatorios para cada participante
+        """# Elegir elementos aleatorios para cada participante
         self.__participantes = []
         for _ in range(n):
             participante = cuerpo(secrets.randbelow(cuerpo.order))
             while participante in self.__participantes or participante == 0:
                 participante = cuerpo(secrets.randbelow(cuerpo.order))
-            self.__participantes.append(participante)
+            self.__participantes.append(participante)"""
 
 
 
-    def crear_anticipadas(self, anticipadas: int):
-        if  not  0 <= anticipadas < self._reconstruccion:
-            raise ValueError(f'El numero de participaciones anticipadas ({anticipadas}) debe estar entre 0 y el parámetro de privacidad ({self._reconstruccion - 1}).')
-        #self.__participaciones_anticipadas = list(zip(gf(np.arange(1, anticipadas + 1)), self._cuerpo.Random(anticipadas)))
-        self.__participaciones_anticipadas = list(zip(self.__participantes[:anticipadas], self._cuerpo.Random(anticipadas)))
+    def crear_anticipadas(self, anticipadas: Sequence[str]) -> list[tuple[str, str]]:
+        if  not  0 <= len(anticipadas) < self._reconstruccion:
+            raise ValueError(f'El numero de participaciones anticipadas ({len(anticipadas)}) debe estar entre 0 y el parámetro de privacidad ({self._reconstruccion - 1}).')
+        aleatoriedad = []
+        for _ in range(len(anticipadas)):
+            aleatoriedad.append(secrets.randbelow(self._cuerpo.order))
+        aleatoriedad_b64 = int_a_b64str(aleatoriedad, 8)
+        self.__participaciones_anticipadas = list(zip(anticipadas, aleatoriedad_b64))
+        #self.__participaciones_anticipadas = list(zip(self.__participantes[:anticipadas], self._cuerpo.Random(anticipadas)))
         return self.__participaciones_anticipadas
 
-    def crear_participaciones(self, secreto: ArrayLike):
-        if self._num_participantes is None:
-            raise AttributeError('No se han definido ningun participante')
+    def crear_participaciones(self, secreto: str) -> list[tuple[Sequence[str], str]]:
+        secreto_i = int.from_bytes(secreto.encode())
         if self.__participaciones_anticipadas is not None:
-            puntos, valores = zip(*self.__participaciones_anticipadas)
-            puntos = np.append(puntos, self._cuerpo(0))
-            valores = np.append(valores, secreto)
+            nombres, valores_b64 = zip(*self.__participaciones_anticipadas)
+            puntos = self._cuerpo(list(self._participantes.index(nombre) + 1 for nombre in nombres) + [0])
+            valores = self._cuerpo(b64str_a_int(valores_b64) + [secreto_i])
             lagrange = galois.lagrange_poly(puntos, valores)
             if lagrange.degree < self._reconstruccion - 1:
                 polinomio = lagrange + Poly.Roots(puntos, field=self._cuerpo)*Poly.Random(self._reconstruccion - len(puntos) - 1, field=self._cuerpo)
             else:
                 polinomio = lagrange
-            #x = np.arange(len(puntos), self._num_participantes + 1)
-            x = self.__participantes[len(puntos) - 1:]
+            x = np.setdiff1d(np.arange(1, len(self._participantes) + 1), puntos)
+            #x = self.__participantes[len(puntos) - 1:]
             del self.__participaciones_anticipadas
         else:
-            polinomio = Poly([*self._cuerpo.Random(self._reconstruccion-1), secreto], field=self._cuerpo)
-            #x = np.arange(1, self._num_participantes + 1)
-            x = self.__participantes
-        return list(zip(self._cuerpo(x), polinomio(x)))
+            polinomio = Poly([*self._cuerpo.Random(self._reconstruccion-1), secreto_i], field=self._cuerpo)
+            x = np.arange(1, len(self._participantes) + 1)
+            #x = self.__participantes
+        participaciones_b64 = gf_a_b64str(polinomio(x), 8)
+        return list(zip(self._cuerpo(x), participaciones_b64))
 
-    def recuperar_secreto_v1(self, participaciones: Sequence[tuple[ArrayLike, ArrayLike]]) -> ArrayLike:
+    def recuperar_secreto_v1(self, participaciones: Sequence[tuple[str, str]]) -> str:
         if len(participaciones) < self._reconstruccion:
             raise ValueError('No se han proporcionado suficientes participaciones para recuperar el secreto')
-        puntos, valores = zip(*participaciones[:self._reconstruccion])
-        puntos = self._cuerpo(puntos)
-        valores = self._cuerpo(valores)
+        nombres, valores_b64 = zip(*participaciones[:self._reconstruccion])
+        puntos = self._cuerpo(list(self._participantes.index(nombre) + 1 for nombre in nombres))
+        valores = self._cuerpo(b64str_a_int(valores_b64))
         polinomio = galois.lagrange_poly(puntos, valores)
-        return polinomio(0)
+        return int_a_str(polinomio(0), 8)
 
-    def recuperar_secreto_v2(self, participaciones: Sequence[tuple[ArrayLike, ArrayLike]]) -> ArrayLike:
+    def recuperar_secreto_v2(self, participaciones: Sequence[tuple[str, str]]) -> str:
         if len(participaciones) < self._reconstruccion:
             raise ValueError('No se han proporcionado suficientes participaciones para recuperar el secreto')
         puntos, valores = zip(*participaciones[:self._reconstruccion])
@@ -82,21 +87,45 @@ class Shamir:
         return np.sum(valores * self._cuerpo(coef))
 
 if __name__ == '__main__':
-    secreto_b = input('Escriba el secreto: ').encode()
-    if len(secreto_b) > 8:
-        print(f'Warning: se han mandado más de 8 bytes, se procede a truncar los primeros 8 bytes')
-        secreto_b = secreto_b[:8]
-    secreto = int.from_bytes(secreto_b)
+    gf = GF(2 ** 64)
 
-    gf = GF(2**64)
-    sh = Shamir(gf ,10, 30)
-    anticipadas = sh.crear_anticipadas(7)
-    participantes = sh.crear_participaciones(gf(secreto))
+    print(type(gf([4238194])))
+    print(int_a_str(gf([461278346283174, 1]), 8))
 
-    conjunto = anticipadas[2:6] + participantes[15:23]
-    sec2 = sh.recuperar_secreto_v2(conjunto)
+    participantes = []
+    r = int(input('Escriba el parámetro de reconstrucción (nº minimo de participantes para recuperar el secreto): '))
+    n = int(input('Escriba el número de particiantes: '))
+    for i in range(n):
+        part = input(f'Escriba el nombre del participante nº{i+1}: ')
+        participantes.append(part)
 
-    secreto_b = int(sec2).to_bytes(8).lstrip(b'\x00')
-    print('secreto:', secreto_b.decode())
+    sh = Shamir(gf, r, participantes)
+
+    yn = input('Desea repartir participación anticipada? (y/n): ')
+    if yn.lower() in ('si', 's', 'y', 'yes'):
+        n_anticipados = int(input('Introduzca el número de participaciones anticipadas: '))
+        anticipadas = []
+        for i in range(n_anticipados):
+            part_ant = input(f'Escriba el nombre del participante anticipado nº{i+1}: ')
+            anticipadas.append(part_ant)
+        participaciones_anticipadas = sh.crear_anticipadas(anticipadas)
+        for nombre, ant_b64 in participaciones_anticipadas:
+            print(f'{nombre}: {ant_b64}')
+
+
+    secreto = input('Escriba el secreto: ')
+    participantes = sh.crear_participaciones(secreto)
+    for nombre, part_b64 in participantes:
+        print(f'{nombre}: {part_b64}')
+
+    print('Escriba el nombre de los participantes que busca reconstruir el secreto y su participacion.')
+    conjunto = []
+    for _ in range(r):
+        nombre = input('Nombre: ')
+        participacion = input('Participacion: ')
+        conjunto.append((nombre, participacion))
+
+    secreto = sh.recuperar_secreto_v1(conjunto)
+    print('secreto:', secreto)
 
 
--- utils/_utils.py
+++ utils/_utils.py
@@ -1,4 +1,31 @@
 from functools import wraps
+from base64 import b64decode, b64encode
+from typing import Sequence
+
+from galois.typing import ArrayLike
+
+def str_a_int():
+    pass
+
+def int_a_str(numero: int | ArrayLike | list[int] , longitud: int) -> list[str] | str:
+    if isinstance(numero, list):
+        return list(n.to_bytes(longitud).lstrip(b'\x00').decode() for n in numero)
+    elif isinstance(numero, int):
+        return numero.to_bytes(longitud).lstrip(b'\x00').decode()
+    else:
+        if numero.ndim > 0:
+            return int(numero).to_bytes(longitud).lstrip(b'\x00').decode()
+
+    return list(n.to_bytes(longitud).lstrip(b'\x00').decode() for n in numero.tolist())
+
+def gf_a_b64str(gf_array: ArrayLike, longitud: int) -> list[str]:
+    return list(b64encode(int(numero).to_bytes(longitud)).decode() for numero in gf_array)
+
+def int_a_b64str(lista_int: Sequence[int], longitud: int) -> list[str]:
+    return list(b64encode(numero.to_bytes(longitud)).decode() for numero in lista_int)
+
+def b64str_a_int(lista_str_b64: list[str]) -> list[int]:
+    return list(int.from_bytes(b64decode(str_b64.encode())) for str_b64 in lista_str_b64)
 
 # def varificar_instancias(funcion):
 #     nombres_argumentos = funcion.__code__.co_varnames
